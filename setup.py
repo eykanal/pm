0872cde675495731c3ead2495ca888883c498428
Eliezer Kanal <eykanal@gmail.com>
2015-03-24 06:55:20 -0400

    removing all CDN links :(

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
     name='django-projManager',
-    version='0.1.2',
+    version='0.1.4',
     packages=find_packages(),
-    install_requires=['django >= 1.7'],
+    install_requires=['django >= 1.7', 'django-crispy-forms', 'django-jsonview'],
     include_package_data=True,
     license='BSD License',  # example license
     description='A Django app for project management.',
